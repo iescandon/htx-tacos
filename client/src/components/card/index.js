66074fcf305d0070f1d309f241f075d6fb39dc78
Inez Escandon <iescan4@gmail.com>
2021-01-30 09:39:17 -0600

    removed image import

--- client/src/components/card/index.js
+++ client/src/components/card/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import './style.css';
-import pic from '../../assets/torchys.jpg';
 
 function CardComponent({ restaurant }) {
 	return (
