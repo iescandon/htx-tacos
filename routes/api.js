47a1af6ec92658c324f39fb5721d2f8f58324b7a
Inez Escandon <iescan4@gmail.com>
2021-01-31 08:11:06 -0600

    add packages and code to server.js

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,8 @@
 const router = require('express').Router();
 const db = require('../models');
 
-// router.get('/api/tacos', (req, res) => {
-// 	db.Users.find({})
-// 		.then((data) => {
-// 			res.json(data);
-// 		})
-// 		.catch((err) => {
-// 			res.status(400).json(err);
-// 		});
-// });
-
 router.get('/', (req, res) => {
-	db.Restaurants.find({})
+	db.Users.find({})
 		.then((data) => {
 			res.json(data);
 		})
@@ -30,64 +20,5 @@ router.get('/api/tacos', (req, res) => {
 			res.status(400).json(err);
 		});
 });
-
-// router.put('/api/trails', (req, res) => {
-// 	console.log(req.body);
-// 	db.TrailSystem.update(
-// 		{ _id: req.body.id },
-// 		{ $set: { open: req.body.status, lastToggled: Date.now() } }
-// 	)
-// 		.then((data) => {
-// 			res.json(data);
-// 			console.log(data);
-// 		})
-// 		.catch((err) => {
-// 			console.log(err);
-// 		});
-// });
-
-// router.put('/api/trails/:id', (req, res) => {
-// 	console.log(req.params.id, req.body);
-// 	db.TrailSystem.updateOne(
-// 		{ _id: req.body.selectedTrail, 'trails.trailId': req.params.id },
-// 		{
-// 			$set: {
-// 				'trails.$.condition': req.body.condition,
-// 				'trails.$.lastUpdated': Date.now(),
-// 			},
-// 		}
-// 	)
-// 		.then((data) => {
-// 			res.json(data);
-// 			console.log(data);
-// 		})
-// 		.catch((err) => {
-// 			console.log(err);
-// 		});
-// });
-
-// router.put('/api/trails/comments/:id', (req, res) => {
-// 	console.log(req.body);
-// 	db.TrailSystem.updateOne(
-// 		{
-// 			_id: req.body.selectedTrail,
-// 			'trails.trailId': req.params.id,
-// 		},
-// 		{
-// 			$set: {
-// 				'trails.$.comment': req.body.trailComment.comment,
-// 				'trails.$.commenter': req.body.trailComment.commenter,
-// 				'trails.$.commentDate': Date.now(),
-// 			},
-// 		}
-// 	)
-// 		.then((data) => {
-// 			res.json(data);
-// 			console.log(data);
-// 		})
-// 		.catch((err) => {
-// 			console.log(err);
-// 		});
-// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
 const express = require('express');
-
+const secure = require('ssl-express-www');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(secure);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/htxtacosDB', {
 	useNewUrlParser: true,
@@ -17,6 +21,11 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/htxtacosDB', {
 });
 
 app.use(require('./routes/api.js'));
+
+if (process.env.NODE_ENV === 'production') {
+	console.log(process.env.REACT_APP_GOOGLE_API_KEY);
+	app.use(express.static('client/build'));
+}
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
